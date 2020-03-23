c0b3c1c34e1ded2c9c8970e48247f1115c409402
Romwel Ace Galarosa <59575937+acegalarosa@users.noreply.github.com>
2020-03-23 11:18:57 -0600

    update

--- SPROCS/E-Stored Procedures Transaction Practice.sql
+++ SPROCS/E-Stored Procedures Transaction Practice.sql
@@ -23,3 +23,76 @@ IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.ROUTINES WHERE ROUTINE_TYPE = N'PROC
 GO
 
 CREATE PROCEDURE DissolveClub
+   @ClubID  varchar(10)
+AS
+
+	IF @ClubID IS NULL
+	BEGIN
+		RAISERROR('ClubID is required (cannot be null)',16,1)
+	END
+	ELSE
+	BEGIN
+		IF NOT EXISTS (SELECT ClubID FROM Club WHERE ClubID = @ClubID)
+		BEGIN
+			RAISERROR('That Club does not exist', 16, 1)
+		END
+		ELSE
+		BEGIN
+			BEGIN TRANSACTION
+			--1) Remove members of the club (from Activity)
+			DELETE FROM Activity WHERE ClubID = @ClubID
+			--Remember to do a check of your global variables to see if there was a problem
+			IF @@ERROR <> 0 -- then there is a problem with the delete, no need to check @@ROWCOUNT
+			BEGIN
+				ROLLBACK TRANSACTION
+				RAISERROR('Unable to remove members from the club', 16, 1)
+			END
+			ELSE
+			BEGIN
+			--2) Remove from Club
+				DELETE FROM Club WHERE ClubID = @ClubID
+				IF @@ERROR <> 0 OR @@ROWCOUNT = 0 -- Check if there's a problem
+				BEGIN
+					ROLLBACK TRANSACTION
+					RAISERROR('Unable to delete the club', 16, 1)
+				END
+				ELSE
+				BEGIN
+					COMMIT TRANSACTION
+				END
+			END
+		END
+	END
+RETURN
+GO
+
+-- Test my stored procedure
+-- SELECT * FROM Club
+-- SELECT * FROM Activity
+EXEC DissolveClub 'CSS'
+EXEC DissolveClub 'NASA1'
+EXEC DissolveClub 'WHA?'
+
+
+
+
+
+
+
+
+
+	--ELSE
+	--BEGIN TRANSACTION
+	--DELETE ClubID FROM Activity WHERE ClubID = @ClubID
+	--IF ClubID NOT EXISTS (SELECT ClubID FROM Activity WHERE ClubID = @ClubID)
+
+	--BEGIN
+	--	RAISERROR('Unable to Delete ClubID' ,16, 1)
+	--ROLLBACK TRANSACTION
+
+
+	
+
+
+
+
